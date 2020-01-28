181836b3fb718faf2a0d9227faad909d0f9d152d
fatima-khelfaoui <fatimakhelfaoui7@gmail.com>
2020-01-28 02:06:35 +0100

    siteweb

---
+++ js/Beni-Mellal.js
@@ -0,0 +1,12 @@
+var ben ={
+"type": "FeatureCollection",
+"name": "Beni-Mellal",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 8, "code_provi": "081", "nom_provin": "AZILAL", "Chomage": 0.041, "Pauvrete": 0.275, "PIBhab": 19415.0, "PIBsec1": 0.17, "PIBsec2": 0.06, "PIBsec3": 0.235 }, "geometry": { "type": "Point", "coordinates": [ -6.535, 31.8912 ] } },
+{ "type": "Feature", "properties": { "gid": 9, "code_provi": "091", "nom_provin": "BENI MELLAL", "Chomage": 0.056, "Pauvrete": 0.062, "PIBhab": 22374.0, "PIBsec1": 0.17, "PIBsec2": 0.06, "PIBsec3": 0.22 }, "geometry": { "type": "Point", "coordinates": [ -6.0182, 32.4694 ] } },
+{ "type": "Feature", "properties": { "gid": 29, "code_provi": "255", "nom_provin": "FQUIH BEN SALLAH", "Chomage": 0.041, "Pauvrete": 0.062, "PIBhab": 21578.0, "PIBsec1": 0.155, "PIBsec2": 0.06, "PIBsec3": 0.225 }, "geometry": { "type": "Point", "coordinates": [ -6.7838, 32.4311 ] } },
+{ "type": "Feature", "properties": { "gid": 37, "code_provi": "301", "nom_provin": "KHENIFRA", "Chomage": 0.056, "Pauvrete": 0.053, "PIBhab": 19572.0, "PIBsec1": 0.045, "PIBsec2": 0.11, "PIBsec3": 0.31 }, "geometry": { "type": "Point", "coordinates": [ -5.7715, 33.0169 ] } },
+{ "type": "Feature", "properties": { "gid": 38, "code_provi": "311", "nom_provin": "KHOURIBGA", "Chomage": 0.081, "Pauvrete": 0.053, "PIBhab": 27415.0, "PIBsec1": 0.0, "PIBsec2": 0.265, "PIBsec3": 0.17 }, "geometry": { "type": "Point", "coordinates": [ -6.5767, 32.925 ] } }
+]
+}
